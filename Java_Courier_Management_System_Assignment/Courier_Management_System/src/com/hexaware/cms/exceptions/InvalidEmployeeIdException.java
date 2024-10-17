bfb918f1966429262ee8239b4bac322dc699001b
Devanshu-189 <sharmadevanshu940@gmail.com>
2024-10-17 12:00:22 +0530

    Add files via upload

---
+++ Java_Courier_Management_System_Assignment/Courier_Management_System/src/com/hexaware/cms/exceptions/InvalidEmployeeIdException.java
@@ -0,0 +1,9 @@
+package com.hexaware.cms.exceptions;
+
+public class InvalidEmployeeIdException extends Exception{
+	
+	public InvalidEmployeeIdException(String message) {
+		super(message);
+	}
+
+}
